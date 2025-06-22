8038bd52f86080ac942bcd378cc29d55c6a2032b
angfxstudio <angraphicstudio.infy.uk@gmail.com>
2025-06-22 23:01:19 +0600

    Update app.py

--- app.py
+++ app.py
@@ -442,4 +442,6 @@ def docs():
 
 # ====== Main ======
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
